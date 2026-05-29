c7ee34f57e1acaaeb0fa57b65c194603f805a372
jyjestudioarquitectonico-source <jyjestudioarquitectonico@gmail.com>
2026-05-29 08:54:29 -0500

    JYJ Firebase sync: sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,3 @@
-const VER='jyj-v6';
-self.addEventListener('install',e=>{e.waitUntil(self.skipWaiting());});
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(function(ks){return Promise.all(ks.map(function(k){return caches.delete(k);}));})
-    .then(function(){return self.clients.claim();})
-    .then(function(){return self.clients.matchAll({includeUncontrolled:true,type:'window'});})
-    .then(function(cls){cls.forEach(function(c){try{c.navigate(c.url);}catch(e){c.postMessage({type:'FORCE_RELOAD'});}});})
-  );
-});
-self.addEventListener('fetch',e=>{
-  var u=new URL(e.request.url);
-  if(u.pathname.endsWith('.html')||u.pathname.endsWith('/')||u.pathname.includes('version.json')){
-    e.respondWith(fetch(e.request,{cache:'no-cache'}).catch(function(){return caches.match(e.request);}));return;
-  }
-  e.respondWith(caches.match(e.request).then(function(h){if(h)return h;return fetch(e.request).then(function(res){if(res&&res.status===200)caches.open(VER).then(function(c){c.put(e.request,res.clone());});return res;});}));
-});
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-cache'}).catch(()=>caches.match(e.request)));});
